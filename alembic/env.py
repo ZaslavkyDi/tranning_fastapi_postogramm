9dfb9e2b8f454517c1d6525a0658185d61eb470a
Dmytro Zaslavskyi <dmytro.zaslavskyi@extrawest.com>
2021-02-23 17:09:22 +0200

    Turn ON alembic autogenerate migration function

--- alembic/env.py
+++ alembic/env.py
@@ -1,12 +1,11 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
+from sqlalchemy import engine_from_config, pool
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
+
 config = context.config
 
 # Interpret the config file for Python logging.
@@ -15,9 +14,10 @@ fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+from app.db.base import Base
+
+target_metadata = Base.metadata
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
---
+++ app/db/base.py
@@ -0,0 +1,7 @@
+"""
+Imports for Alembic
+"""
+from app.db.base_class import Base
+from app.models.user import User
+
+
